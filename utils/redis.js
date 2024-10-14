9b07b916660b7017934730399d3f5b4eb836f7f9
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-10-14 16:28:44 +0100

    push

---
+++ utils/redis.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+import { createClient } from "redis";
+
+class RedisClient {
+    constructor() {
+        this.client = createClient();
+        this.client.on("error", (error) => {
+            console.error(error);
+        });
+    }
+
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (err, reply) => {
+                if (err) reject(err);
+                resolve(reply);
+            });
+        });
+    }
+
+    async set(key, value, duration) {
+        return new Promise((resolve, reject) => {
+            this.client.setex(key, duration, value, (err, reply) => {
+                if (err) reject(err);
+                resolve(reply);
+            });
+        });
+    }
+
+    async del(key) {
+        return new Promise((resolve, reject) => {
+            this.client.del(key, (err, reply) => {
+                if (err) reject(err);
+                resolve(reply);
+            });
+        });
+    }
+}
+
+const redisClient = new RedisClient();
+
+//redisClient.client.connect();
+
+export default redisClient;
