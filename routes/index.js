2d63b89f5df53d388897fc7a46b04ac3b61c1ab3
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-10-17 11:01:13 +0100

    ok

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = require('express').Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.get('/connect', AuthController.getDisconnect);
+router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.post('/users', UsersController.postNew);
 router.get('/users/me', UsersController.getMe);
